dda7f23d238c1e64570562b85f75955c4e36b937
unusali1 <yali71507@gmail.com>
2023-07-26 00:14:19 +0600

    nextjs product cart

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,22 @@
+"use client";
+"use client";
+import Link from 'next/link';
+import React from 'react'
+import { useSelector } from 'react-redux';
+
+
+const Navbar = () => {
+   const item = useSelector((state)=>state.cart);
+  return (
+    <div className='navbar'>
+         <span>Redux Nextjs-13.4</span>
+         <div>
+            <Link className='navLink' style={{fontWeight:"bolder"}} href={"/"}>Home</Link>
+            <Link className='navLink' style={{fontWeight:"bolder",marginRight:"7px"}} href={"/cart"}>Cart</Link>
+            <span style={{fontWeight:"bolder"}}>items:{item.length}</span>
+         </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/app/page.js
@@ -0,0 +1,41 @@
+"use client";
+import React,{useState,useEffect} from 'react'
+import { add } from '@/Redux/CartSlice';
+import { useDispatch } from 'react-redux';
+
+const Homepage = () => {
+  const [products ,setproducts]=useState([])
+ const dispatch = useDispatch();
+
+  const getproducts =async()=>{
+    const res =await fetch("https://fakestoreapi.com/products");
+    const data =await res.json();
+    setproducts(data);
+  }
+
+  const handleadd =(product)=>{
+      dispatch(add(product));
+  }
+
+
+
+  useEffect(()=>{
+   getproducts();
+  },[])
+  return (
+    <div className='productsWrapper'>
+          {
+            products.map((product)=>(
+              <div key={product.id} className='card'>
+                  <img src={product.image} alt='img'/>
+                  <h4>{product.title}</h4>
+                  <h5>{product.price}</h5>
+                  <button className='btn' onClick={()=>handleadd(product)}>Add to cart</button>
+              </div>
+            ))
+          }
+    </div>
+  )
+}
+
+export default Homepage
